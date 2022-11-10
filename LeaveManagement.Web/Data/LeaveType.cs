32f2b7c4de7fa6d3018eb56a2fb4b9af8ea753ca
Curwen Arthurs <curwenarthurs@gmail.com>
2022-11-10 16:55:47 -0600

    Added Database Tables and Migrations, End Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+    }
+}
